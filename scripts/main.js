af0166addbb301ec0c6af3453b19db936d8a9000
julien-ba <julienbarbier42@gmail.com>
2024-09-18 11:18:37 +0200

    setup main filter

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,34 @@
 import { getRecipes } from './api/fetch_recipes.js';
 import { Recipe } from './template/recipes.js';
 
+function filterRecipes(recipes) {
+    const filteredRecipes = [];
+    recipes.forEach(recipe => {
+        filteredRecipes.push(recipe);
+    });
+    return filteredRecipes;
+}
+
 function displayRecipes(recipes) {
     const container = document.querySelector('.cards');
-
     recipes.forEach(recipe => {
         const recipeTemplate = new Recipe(recipe);
         container.appendChild(recipeTemplate.getRecipeDOM());
     });
 }
 
+function displayFilters(recipes) {
+    const container = document.querySelector('.filters-lists');
+    recipes.forEach(recipe => {
+
+    });
+}
+
 async function init() {
     const recipes = await getRecipes();
-    displayRecipes(recipes);
+    const filteredRecipes = filterRecipes(recipes);
+    displayRecipes(filteredRecipes);
+    displayFilters(filteredRecipes);
 }
 
 init();
@@ -23,5 +39,6 @@ searchbox.addEventListener('keyup', search);
 function search(event) {
     if (event.target.value.length > 2) {
         console.log(`search edited: ${event.target.value}`);
+        init();
     }
 }
